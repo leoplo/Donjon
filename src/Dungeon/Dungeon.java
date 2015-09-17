536dcb7dc0a7197c22e258dd9563d16856f6499b
Geneau Leo-paul <geneau@a10p25.fil.univ-lille1.fr>
2015-09-17 16:06:10 +0200

    creation of classes : Room and Dungeon

--- src/Dungeon/Dungeon.java
+++ src/Dungeon/Dungeon.java
@@ -4,41 +4,24 @@ import java.util.*;
 
 public class Dungeon {
 	protected Room currentRoom = new Room();
-	protected boolean gameIsFinished = false;
 	protected final Scanner scanner = new Scanner(System.in);
 
 	public String getCurrentRoom() {
 		return currentRoom;
 	}
 
+	/**
+	 * Interprets the user's command and executs it
+	 * @param command the user's command
+	 */
 	public void interpretCommand(String command) {
-	switch (command) {
-		case "go north":
-		switch (currentRoom) {
-			case "entrance":
-				currentRoom = "intersection";
-				break;
-			case "intersection":
-				currentRoom = "exit";
-				break;
+		try {
+			Room nextRoom = this.currentRoom.goRoom(command);
+			this.currentRoom = nextRoom;
+		} catch (InvalidParameterException e) {
+			System.out.println("You can't go this way");
 		}
-		break;
-	
-		case "go west":
-		switch (currentRoom) {
-			case "intersection":
-			currentRoom = "trap";
-			break;
-			case "entrance":
-			System.out.println("Can’t go west!");
-			break;
-		}
-		break;
-	
-		default:
-		System.out.println("I don’t know what you mean");
 	}
-}
 
 	public static void main(String[] args) {
 		Dungeon dungeon = new Dungeon();
@@ -47,30 +30,21 @@ public class Dungeon {
 
 	public void start() {
 		do {
-			System.out.println("You are in " + getCurrentRoom());
-			System.out.println("What do you want to do?");
+			System.out.println("You are in a new room");
+			System.out.println("What do you want to do now ?");
 			System.out.print("> ");
-// Read a command from the user (stdin)
-			String line = scanner.nextLine();
+			// Read a command from the user (stdin)
+			String line = this.scanner.nextLine();
 			interpretCommand(line);
 		} while (!gameIsFinished());
-		System.out.println("You are in " + getCurrentRoom());
 		if (gameIsWon()) {
-			System.out.println("You win!");
+			System.out.println("You win !");
 		} else {
-			System.out.println("You loose!");
+			System.out.println("You loose !");
 		}
 	}
 	
 	public boolean gameIsFinished() {
-		return gameIsLost() || gameIsWon();
-	}
-
-	public boolean gameIsLost() {
-		return currentRoom.equals("trap");
-	}
-	
-	public boolean gameIsWon() {
-		return currentRoom.equals("exit");
+		return  this.currentRoom.isWinningRoom() || this.currentRoom.isLosingRoom();
 	}
 }
--- src/Dungeon/Room/Room.java
+++ src/Dungeon/Room/Room.java
@@ -1,35 +1,63 @@
 package Dungeon.Room;
 
-public class Room{
+import java.util.*;
 
-	protected String name;
-	protected Room northRoom;
-	protected Room westRoom;
+/**
+ * The class <code>Room</code> represents a Dungeon room.
+ */
+
+public class Room {
+
+	// Attributs
+	protected Map<String,Room> rooms;
 	protected boolean gameIsLost;
 	protected boolean gameIsWin;
 	
-	
-	public Room() {
-		Room(null, null);
-	}
-	
-	public Room (Room northRoom, Room westRoom) {
-		this.northRoom = northRoom;
-		this.westRoom = westRoom;
+	/**
+	 * the Constructer, by default both boolean are initialized to <code>false</code>.
+	 */
+	public Room () {
+		this.rooms = new HashMap<String,Room>();
 		this.gameIsLost = false;
 		this.gameIsWin = false;
 	}
 	
-	public String getName() {
-		return this.name;
+	/**
+	 * This fonction adds a room with a specified direction to <code>this</code> room. <br/>
+	 * If the direction has already been set, the new value replaces it.
+	 * @param direction the direction associated to the room
+	 * @param room the the room associated to the direction 
+	 */
+	public void addRoom (String direction, Room room) {
+		this.rooms.put(direction, room);
 	}
 	
-	public Room goNorthRoom() {
-		
-		return;
+	/**
+	 * This fonction returns the room associated to the chosen direction
+	 * @param direction the chosen direction
+	 * @exception InvalidParameterException if the direction doesn't exist
+	 * @return the room associated to the chosen direction
+	 */
+	public Room goRoom (String direction) throws InvalidParameterException {
+		Room room = this.rooms.get(direction);
+		if (room == null)
+			throw new InvalidParameterException();
+		return room;
 	}
 	
-	public Room goWestRoom() {
-		
-		return;
+	/**
+	 * Check if the room is a winning room
+	 * @return <code>true</code> if the room is a winning room, <code>false</code> else.
+	 */
+	public boolean isWinningRoom () {
+		return this.gameIsWin;
 	}
+	
+	/**
+	 * Check if the room is a losing room
+	 * @return <code>true</code> if the room is a losing room, <code>false</code> else.
+	 */
+	public boolean isLosingRoom () {
+		return this.gameIsLost;
+	}
+}
