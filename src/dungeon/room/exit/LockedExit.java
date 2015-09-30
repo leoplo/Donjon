8c9081f876869e87247df4051e3fd4db9d9492b8
AymericDu <aymeric.ducroquetz@etudiant.univ-lille1.fr>
2015-09-30 22:53:11 +0200

    test Exit and subclass

--- src/dungeon/room/exit/LockedExit.java
+++ src/dungeon/room/exit/LockedExit.java
@@ -3,7 +3,12 @@ package dungeon.room.exit;
 import dungeon.unit.Player;
 
 public abstract class LockedExit extends Exit {
-
+	
+	public LockedExit (boolean isHidden) {
+		super(isHidden);
+		this.isLocked = true;
+	}
+	
 	public boolean unlock(Player player){
 		this.isLocked = false;
 		return true;
